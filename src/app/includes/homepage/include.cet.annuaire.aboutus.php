ab73a66d7b7f166e2b7ba5a97385d5207a913f09
duyminhvu <96867085+duyminhvu@users.noreply.github.com>
2022-02-18 10:12:08 +0100

    Correction des textes de la page Qui sommes-nous ?

--- src/app/includes/homepage/include.cet.annuaire.aboutus.php
+++ src/app/includes/homepage/include.cet.annuaire.aboutus.php
@@ -1,39 +1,38 @@
 <section>
     <div class="row d-xl-flex justify-content-xl-center aboutus--bg">
         <div class="col-12 col-xl-3 d-flex justify-content-center align-items-center">
-            <img class="w-75 w-xl-auto mw-100" src="res/content/images-site/a-propos.png" alt="Max-width 100%" >
+            <img class="w-75 w-xl-auto mw-100" src="res/content/images-site/qui-sommes-nous.png" alt="Max-width 100%" >
         </div>
     </div>
     <div class="row d-flex justify-content-center mb-5">
         <div class="col-5 col-xl-3 d-flex align-items-center">
             <img class="mw-100" src="res/documentation/images-site/castillon-la-bataille.png" alt="">
         </div>
-        <div class="col-12 col-md-6 col-lg-6 col-xl-6 d-flex flex-column text-aboutus">
+        <div class="col-12 col-md-6 col-lg-6 col-xl-4 d-flex flex-column text-aboutus">
             <h6>Castillonnais en transition</h6>
             <p>
                 L’association Castillonnais en transition est composée de girondins et<br>
                 périgourdins engagés. Elle travaille au développement de la BIO Locale.<br>
             </p>
             <h6>Nos priorités sont de :</h6>
-            <p>▸ Remettre l'agriculture, l'alimentation, le vivant, à une place centrale.</p>
-            <p>▸ Renforcer la résilience alimentaire de nos territoires.</p>
-            <p>▸ Créer du lien, de la solidarité et de l'entraide entre<br> producteurs, entre
-                producteurs et consommateurs, avec les collectivités locales engagées, les<br>
+            <p>▸ Remettre l'agriculture, l'alimentation, le vivant, à une place centrale</p>
+            <p>▸ Renforcer la résilience alimentaire de nos territoires</p>
+            <p>▸ Créer du lien, de la solidarité et de l'entraide entre producteurs, entre
+                producteurs et consommateurs, avec les collectivités locales engagées, les
                 associations, les chercheurs...
             </p>
             <h6>Decidelabiolocale.org</h6>
             <p>
-                À partir de ces priorités nous travaillons sur un projet de plate forme<br>
+                À partir de ces priorités nous avons mis en place une plateforme
                 numérique financée par le Conseil régional de Nouvelle Aquitaine et l'Europe.
             </p>
-            <h6>Cet annuaire a un double objectif:</h6>
+            <h6>Cet plateforme a un double objectif :</h6>
             <p>▸ Être exhaustive, en présentant gratuitement tout les producteurs BIO et
-                y tendant qui le souhaitent, dans un rayon de 40 km autour de Castillon la<br>
-                Bataille.<span>En savoir plus sur le BIO et les producteurs y tendant.</span></p>
+                y tendant qui le souhaitent, dans un rayon de 40 km autour de Castillon la
+                Bataille.<span>En savoir plus sur le BIO et les producteurs y tendant</span></p>
             <p>
-                ▸ Être construite à partir de vos besoins et attentes. Le questionnaire ci-
-                dessous va nous permettre de recueillir ces informations qui nous permettront<br>
-                de valoriser votre ferme, votre travail et de connaître vos attentes.
+                ▸ Être construite à partir des besoins et attentes des producteurs. Le questionnaire ci-dessous va nous permettre de recueillir ces informations qui nous permettront
+                de valoriser leur ferme, leur travail et de connaître leurs attentes
             </p>
         </div>
     </div>
