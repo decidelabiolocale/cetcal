4748384edd5fb5c2801422d3855c2b8a1bfac73c
duyminhvu <96867085+duyminhvu@users.noreply.github.com>
2022-04-10 19:45:50 +0200

    Affichage table de résultats

--- src/scripts/js/cetcal/cetcal.min.recherche.avancee.js
+++ src/scripts/js/cetcal/cetcal.min.recherche.avancee.js
@@ -39,8 +39,16 @@ function fetchResultatsRechercheAvancee(json_rav) {
       var pk_rav_producteurs = [];
       var pk_rav_entites = [];
       var pks = [];
+      // Table de résultats - Initialisation de variables
+      var table_resultats = '';
+      var ligne_table = '';
+      var n = 0;
+      // Table de résultats - Fin initialisation de variables
       $('#resultats-recherche-avancee').empty(); 
-      $('#resultats-recherche-avancee').append((data.length + data_entites.length <= 0) ? '<p class="detail-rav-resultats">Aucun résultat.</p>' : '<p class="detail-rav-resultats">' + (data.length + data_entites.length) + ' résultats trouvés.</p>'); 
+      $('#resultats-recherche-avancee').append((data.length + data_entites.length <= 0) ? '<p class="detail-rav-resultats">Aucun résultat.</p>' : '<p class="detail-rav-resultats"><a href="src/scripts/js/cetcal/cetcal.table.resultats.html">' + (data.length + data_entites.length) + ' résultats trouvés.</a></p>'); 
+      // Table de résultats - Début et en tête de table
+      table_resultats = '<table><tr><th>Num</th><th>Entité</th><th>Commune/Adresse</th><th>Type</th><th>Nom</th><th>Prénom</th><th>Email</th><th>Tel 1</th><th>Tel 2</th></tr>';
+      // Table de résultats - Fin début et en tête de table
       for (var i in data) {
         resultats.push([data[i].nomferme, 
           '<div id="bloc-rav-' + data[i].pk + '" class="bloc-rav-element-resultat">'
@@ -59,6 +67,19 @@ function fetchResultatsRechercheAvancee(json_rav) {
             + '</div>']);
         pk_rav_producteurs.push(data[i].pk);
         pks.push(data[i].pk);
+        // Table de résultats - Ecriture d'une ligne de résultat producteur  
+        n++;
+        ligne_table = '<tr><td>' + n + '</td><td>' + data[i].nomferme + '</td>';
+        ligne_table = ligne_table + '<td>' + ((data[i].prodInscrit === 'false') ? data[i].adrfermeLtrl : data[i].adrCommune) + '</td>';
+        ligne_table = ligne_table + '<td>' + ((data[i].prodInscrit === 'false') ? '' : data[i].typeDeProduction.replaceAll('µ', ' ')) + '</td>';
+        ligne_table = ligne_table + '<td>' + data[i].nom + '</td>';
+        ligne_table = ligne_table + '<td>' + data[i].prenom + '</td>';
+        ligne_table = ligne_table + '<td>' + data[i].email + '</td>';
+        ligne_table = ligne_table + '<td>' + data[i].telfix + '</td>';
+        ligne_table = ligne_table + '<td>' + data[i].telport + '</td>';
+        ligne_table = ligne_table + '</tr>';
+        table_resultats = table_resultats + ligne_table;
+        // Table de résultats - Fin ecriture d'une ligne de résultat producteur 
       }
 
       for (var i in data_entites) {
@@ -77,6 +98,19 @@ function fetchResultatsRechercheAvancee(json_rav) {
             + '</div>']);
         pk_rav_entites.push(data_entites[i].pk);
         pks.push(data_entites[i].pk);
+        // Table de résultats - Ecriture d'une ligne de résultat entité 
+        n++;
+        ligne_table = '<tr><td>' + n + '</td><td>' + data_entites[i].denomination + '</td>';
+        ligne_table = ligne_table + '<td>' + data_entites[i].adresse + '</td>';
+        ligne_table = ligne_table + '<td>' + data_entites[i].typeLibelle + '</td>';
+        ligne_table = ligne_table + '<td>' + data_entites[i].personne + '</td>';
+        ligne_table = ligne_table + '<td></td>';
+        ligne_table = ligne_table + '<td>' + data_entites[i].email + '</td>';
+        ligne_table = ligne_table + '<td>' + data_entites[i].tels + '</td>';
+        ligne_table = ligne_table + '<td></td>';
+        ligne_table = ligne_table + '</tr>';
+        table_resultats = table_resultats + ligne_table;
+        // Table de résultats - Fin ecriture d'une ligne de résultat entité 
       }
 
       resultats = resultats.sort(comparator);
@@ -89,6 +123,9 @@ function fetchResultatsRechercheAvancee(json_rav) {
       scrollTowardsId('cet-annuaire-crt-main-container', 2);
       updateCartoRechercheAvancee(pk_rav_producteurs, pk_rav_entites, json_rav.commune, json_rav.rayon);
       focusOnBounds(pks);
+      // Table de résultats - Fin de table et écriture en session
+      table_resultats = table_resultats + '</table>';
+      sessionStorage.setItem("tableResultats", table_resultats);
     },
     error: function(jqXHR, textStatus, errorThrown) {
       $('#resultats-recherche-avancee').empty();
